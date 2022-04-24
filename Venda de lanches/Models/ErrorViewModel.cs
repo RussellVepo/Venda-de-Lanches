b45d84c262ff5d08037b79a17999c65ab6414ed8
Russell Melo Vepo <russellvepo@gmail.com>
2022-04-24 12:53:17 -0300

    Repositório criado

---
+++ Venda de lanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Venda_de_lanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
