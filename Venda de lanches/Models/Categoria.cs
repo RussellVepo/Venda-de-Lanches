8613e7e992a0463715601de6e57f2764a4c77847
Russell Melo Vepo <russellvepo@gmail.com>
2022-04-28 18:59:09 -0300

    Create Class Lanche/ Categoria to Models

---
+++ Venda de lanches/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace Venda_de_lanches.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+       public string CategoriaName { get; set; }
+
+        public string Descricao { get; set; }
+        public List<Lanche> Lanches { get; set; }
+    }
+}
