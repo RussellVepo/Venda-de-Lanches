403f986799e997b7023948dd6b3cfbd3cb67c202
Russell Melo Vepo <russellvepo@gmail.com>
2022-04-29 19:46:58 -0300

    Criando Folder Context

---
+++ Venda de lanches/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Venda_de_lanches.Models;
+
+namespace Venda_de_lanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options): base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche>lanches  { get; set; }
+    }
+}   
+
+
